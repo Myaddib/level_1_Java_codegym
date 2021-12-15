bb70fbf9254f1da6c5e4699385bd8dba7151c594
Myaddib <Hotabich80@gmail.com>
2021-12-15 09:34:05 +0200

    Codegym Level 1, Lesson 3, Basic Java

---
+++ src/basic/java_codegym/Main.java
@@ -0,0 +1,59 @@
+package basic.java_codegym;
+
+public class Main {
+    int a;
+    int b;
+
+    public static void main(String[] args) {
+// write your code here
+        System.out.println("Robots are friends to humans");
+
+        System.out.println("Humans and robots are friends forever");
+        System.out.println("Humans and robots are friends forever");
+        System.out.println("Humans and robots are friends forever");
+        System.out.print("1");
+        System.out.println();
+        System.out.println();
+// task_1
+        //Amigo
+        //Is The
+        //Best
+        System.out.println("Amigo");
+        System.out.println("Is The");
+        System.out.println("Best");
+        System.out.println();
+        System.out.println();
+
+// task_2
+        //AmigoIs The
+        //Best
+        System.out.print("Amigo");
+        System.out.println("Is The");
+        System.out.print("Best");
+        System.out.println();
+        System.out.println();
+
+// task_3
+        //AmigoIs TheBest
+        System.out.print("Amigo");
+        System.out.print("Is The");
+        System.out.print("Best");
+        System.out.println();
+        System.out.println();
+
+// task_4
+        //Amigo Is The Best.
+        System.out.print("Amigo ");
+        System.out.print("Is The ");
+        System.out.print("Best");
+        System.out.println();
+        System.out.println();
+
+//Homework
+        System.out.print( "I think being a programmer is cool");
+    }
+
+    public static double pi() {
+        return 3.14;
+    }
+}
